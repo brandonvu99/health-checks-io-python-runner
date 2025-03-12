73f38ced84866a980abf9b4b0f21ab39124a0ee9
Brandon Vu <brandonexxod@gmail.com>
2025-03-12 19:22:27 -0400

    expose ScriptStatus, not HealthChecksPingType directly in the __init__.py

--- health_checks_io_runner/__init__.py
+++ health_checks_io_runner/__init__.py
@@ -1,4 +1,4 @@
-"""Package for HealthChecksIoRunner and HealthChecksPingType."""
+"""Package for HealthChecksIoRunner and ScriptStatus."""
 
 from health_checks_io_runner.health_checks_io_runner import HealthChecksIoRunner
-from health_checks_io_runner.health_checks_io_runner import HealthChecksPingType
+from health_checks_io_runner.health_checks_io_runner import ScriptStatus
